86603dd8ec220b404bcbc6bbbc2ad9ab58c3ac54
Iaannn06 <alfianazad05@gmail.com>
2024-05-13 10:41:52 +0700

    menampilkan detail dari mahasiswa

--- MemberStatic/MemberStatic.cpp
+++ MemberStatic/MemberStatic.cpp
@@ -24,4 +24,24 @@ void mahasiswa::setID()
 {
 	id = ++nim;
 }
+void mahasiswa::printAll()
+{
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
 
+int main()
+{
+	mahasiswa mhs1("lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("Andi Kurniawan");
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
+
+}
