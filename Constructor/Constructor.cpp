6f1fdf2791fff904900b7610476e3fc3699b87d7
Iaannn06 <alfianazad05@gmail.com>
2024-05-13 12:43:21 +0700

    membuat constructor

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -20,10 +20,28 @@ public:
 	Mahasiswa(string iNama) {
 		nama = iNama;
 	}
-	Mahasiswa(int iNim, string iNama);
-	void cetak();
+	Mahasiswa(int iNim, string iNama) {
+	nim=iNim;
+	nama= iNama;
+	}
 
+	void cetak() {
+		cout << "Nim = " << nim << endl;
+		cout << "Nama = " << nama << endl;
+		cout << endl;
+	}
 
 };
 
+int main() {
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(20);
+	Mahasiswa mhs3("Michael Phillips");
+	Mahasiswa mhs4(20,"Michael Phillips");
 
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+	return 0;
+}
---
+++ Deconstructor/Deconstructor.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+Class angka{
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); 
+}
